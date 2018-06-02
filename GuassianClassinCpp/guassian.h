34bbdecfa82e5d94309430389e71fa3a4d5a7b7e
Huaiyi Zhao <huaiyizhao@gmail.com>
2018-06-02 15:02:11 +0800

    Add files via upload

---
+++ GuassianClassinCpp/guassian.h
@@ -0,0 +1,30 @@
+# ifndef GUASSIAN_H
+# define GUASSIAN_H
+# include <math.h>       /* sqrt, exp */
+
+// class declaration
+class Gaussian
+{
+	private:
+		float mu, sigma2;
+
+	public:
+		
+		// constructor functions
+		Gaussian ();
+		Gaussian (float, float);
+
+		// change value of average and standard deviation 
+		void setMu(float);
+		void setSigma2(float);
+
+		// output value of average and standard deviation
+		float getMu();
+		float getSigma2();
+
+		// functions to evaluate 
+		float evaluate (float);
+		Gaussian mul (Gaussian);
+		Gaussian add (Gaussian);
+};
+# endif /* guassian.h */
